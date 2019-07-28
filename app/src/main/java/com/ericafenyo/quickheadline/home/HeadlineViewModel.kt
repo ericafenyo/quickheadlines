3a6d81bde6e3ed8e24caa46a95d8bbb773de7fff
ericafenyo <ericafenyo@yahoo.com>
2019-07-28 17:52:07 +0200

    load and emit data

--- app/src/main/java/com/ericafenyo/quickheadline/home/HeadlineViewModel.kt
+++ app/src/main/java/com/ericafenyo/quickheadline/home/HeadlineViewModel.kt
@@ -2,5 +2,17 @@
 
 package com.ericafenyo.quickheadline.home
 
-class HeadlineViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import com.ericafenyo.data.entities.Article
+import com.ericafenyo.quickheadline.ScopedViewModel
+import com.ericafenyo.repository.NewsRepositoryImpl
+
+class HeadlineViewModel : ScopedViewModel() {
+    private val repository = NewsRepositoryImpl()
+
+    val article: LiveData<List<Article>> = liveData {
+        val data = repository.getTopStories("home")
+        emit(data)
+    }
 }
